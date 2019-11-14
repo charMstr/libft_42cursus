f4df20cf536362e173faac3402cb9c75a4b854c8
Charlie ARMSTRONG <charmstr@e2r7p12.42.fr>
2019-11-14 17:58:01 +0100

    finally finished with a working ft_strlcat function

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: charmstr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 22:09:21 by charmstr          #+#    #+#             */
-/*   Updated: 2019/11/12 14:33:08 by charmstr         ###   ########.fr       */
+/*   Updated: 2019/11/14 17:54:48 by charmstr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,24 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t i;
-	size_t len_dst;
-	size_t len_src;
+	size_t k;
+	size_t src_len;
 
-	len_dst = ft_strlen(dst);
-	len_src = ft_strlen(src);
 	i = 0;
-	if (dstsize == 0)
-		return (len_src);
-	while (i < len_dst)
-	{
+	k = 0;
+	src_len = 0;
+	while (dst[i] && i < dstsize)
 		i++;
-	}
-	while (i < dstsize - 1 && (i - len_dst) < len_src)
+	while (src[src_len])
+		src_len++;
+	if (!dstsize)
+		return (i + src_len);
+	while (src[k] && (i + k < dstsize - 1))
 	{
-		*(dst + i) = *(src + i - len_dst);
-		i++;
+		dst[i + k] = src[k];
+		k++;
 	}
-	*(dst + i) = '\0';
-	if (len_src + dstsize < len_src + len_dst)
-		return (len_src + dstsize);
-	else
-		return (len_src + len_dst);
+	if (i + k <= dstsize - 1)
+		dst[i + k] = '\0';
+	return (i + src_len);
 }
--- test_libft_mains/ft_strlcat_main.c
+++ test_libft_mains/ft_strlcat_main.c
@@ -6,7 +6,7 @@
 /*   By: charmstr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 22:09:21 by charmstr          #+#    #+#             */
-/*   Updated: 2019/11/14 00:03:02 by charmstr         ###   ########.fr       */
+/*   Updated: 2019/11/14 17:56:07 by charmstr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,55 +15,72 @@
 #include <string.h>
 
 
-
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t i;
-	size_t len_dst;
-	size_t len_src;
+	size_t k;
+	size_t src_len;
 
-	len_dst = ft_strlen(dst);
-	len_src = ft_strlen(src);
 	i = 0;
-	if (dstsize == 0)
-		return (len_src);
-	while (i < len_dst && i < dstsize)
+	k = 0;
+	src_len = 0;
+	while (dst[i] && i < dstsize)
 		i++;
-	while (i < dstsize - 1 && (i - len_dst) < len_src)
+	while (src[src_len])
+		src_len++;
+	if (!dstsize)
+		return (i + src_len);
+	while (src[k] && (i + k < dstsize - 1))
 	{
-		*(dst + i) = *(src + i - len_dst);
-		i++;
+		dst[i + k] = src[k];
+		k++;
 	}
-	*(dst + i) = '\0';
-	if (len_src + dstsize < len_src + len_dst)
-		return (len_src + dstsize);
-	else
-		return (len_src + len_dst);
+	if (i + k <= dstsize - 1)
+		dst[i + k] = '\0';
+	return (i + src_len);
 }
 
 int	main(int argc, char *argv[])
 {
 	char *dst1 ;
 	char *dst2;
+	char *src;
+	int dst_size;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
-		printf("failed to provide a src string as argument\n");
+		printf("failed to provide a src string as argument, and destsize\n");
 		return (0);
 	}
-	char *src = argv[1];
-	if (!(dst1 = (char *)malloc(sizeof(char) * 15)))
+	src = argv[1];
+	dst_size = atoi(argv[2]);
+	if (!(dst1 = (char *)malloc(sizeof(char) * dst_size)))
 		return (0);
-	if (!(dst2 = (char *)malloc(sizeof(char) * 15)))
+	if (!(dst2 = (char *)malloc(sizeof(char) * dst_size)))
 		return (0);
+	memset(dst1, 0, dst_size);
+	memset(dst2, 0, dst_size);
+	memset(dst1, 'r', 6);
+	memset(dst2, 'r', 6);
 
-	memset(dst1, 'r', 15);
-	//printf("%zu\n",strlcat(dst1, "lorem ipsum dolor sit amet", 5));
+	printf("\nft_str...:%zu",ft_strlcat(dst1, src, dst_size));
+	write(1, "\n", 1);
 	write(1, dst1, 15);
-
-	printf("\n");
-	memset(dst2, 'r', 15);
-	printf("%zu\n",ft_strlcat(dst2, "lorem ipsum dolor sit amet", 5));
+	write(1, "\n", 1);
+	printf("str...:%zu",strlcat(dst2, src, dst_size));
+	write(1, "\n", 1);
 	write(1, dst2, 15);
-	return (0);
+	write(1, "\n", 1);
+
+	memset(dst1, 0, dst_size);
+	memset(dst2, 0, dst_size);
+	memset(dst1, 'r', 6);
+	memset(dst2, 'r', 6);
+
+	dst1[14] = 'a';
+	dst2[14] = 'a';
+	printf("\nft_str...:%zu\n",ft_strlcat(dst1, src, dst_size));
+	write(1, dst1, 15);
+	printf("\nstr...:%zu\n",strlcat(dst2, src, dst_size));
+	write(1, dst2, 15);
 }
