0beaec0814bca852a7dfc2dd53f7a444b30d8c3c
Charlie ARMSTRONG <charmstr@e2r7p10.42.fr>
2019-11-15 00:32:19 +0100

    modified the actual philosophy of the ft_lstmap function

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,31 +6,38 @@
 /*   By: charmstr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 19:46:31 by charmstr          #+#    #+#             */
-/*   Updated: 2019/11/11 21:04:59 by charmstr         ###   ########.fr       */
+/*   Updated: 2019/11/15 00:31:08 by charmstr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** note: del function is optional. should be NULL if not used.
+** note: iteration of (*f) on each link. see ft_lstiter if no copy wanted.
 **
-** note2: iteration of f on each link. see ft_lstiter if no copy wanted.
+** note2: (*del) is passed to ft_lstclear to clean the whole new_list if a
+** malloc failed in ft_lstnew.
 **
-** RETURN: copy of lst which is modified according to (*f) function.
+** RETURN: copy of lst which is modified according to (*f) function. The RETURN
+** can be NULL if lst is NULL or if failure mallocing midway
 */
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list *new_list;
+	t_list *new_link;
 
 	new_list = NULL;
 	while (lst)
 	{
 		if (*f)
-			ft_lstadd_back(&new_list, ft_lstnew((*f)(lst->content)));
-		if (*del)
-			(*del)(lst->content);
+			new_link = ft_lstnew((*f)(lst->content));
+		if (!new_link && (*del))
+		{
+			ft_lstclear(&new_list, (*del));
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_link);
 		lst = lst->next;
 	}
 	return (new_list);
--- test_libft_mains/ft_lstmap_main.c
+++ test_libft_mains/ft_lstmap_main.c
@@ -6,7 +6,7 @@
 /*   By: charmstr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 19:46:31 by charmstr          #+#    #+#             */
-/*   Updated: 2019/11/11 21:03:09 by charmstr         ###   ########.fr       */
+/*   Updated: 2019/11/15 00:29:58 by charmstr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,31 @@
 #include <stdio.h>
 
 /*
-** note: del function is optional. should be NULL if not used.
+** note: iteration of (*f) on each link. see ft_lstiter if no copy wanted.
 **
-** note2: iteration of f on each link. see ft_lstiter if no copy wanted.
+** note2: (*del) is passed to ft_lstclear to clean the whole new_list if a
+** malloc failed in ft_lstnew.
 **
-** RETURN: copy of lst which is modified according to (*f) function.
+** RETURN: copy of lst which is modified according to (*f) function. The RETURN
+** can be NULL if lst is NULL or if failure mallocing midway
 */
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list *new_list;
+	t_list *new_link;
 
 	new_list = NULL;
 	while (lst)
 	{
 		if (*f)
-			ft_lstadd_back(&new_list, ft_lstnew((*f)(lst->content)));
-		if (*del)
-			(*del)(lst->content);
+			new_link = ft_lstnew((*f)(lst->content));
+		if (!new_link && (*del))
+		{
+			ft_lstclear(&new_list,(*del));
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_link);
 		lst = lst->next;
 	}
 	return (new_list);
@@ -65,7 +72,7 @@ void	*func1(void *content)
 
 void	del(void *content)
 {
-	;
+	content = (void *)content;
 }
 
 int	main()
@@ -84,7 +91,7 @@ int	main()
 	ft_lstadd_back(&head, ft_lstnew(quatro));
 	display(head, 1);
 
-	copy = ft_lstmap(head, func1, NULL);
+	copy = ft_lstmap(head, func1, del);
 	display(copy, 2);
 	display(head, 1);
 	return (0);
